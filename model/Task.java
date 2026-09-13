316529ad451595d3e113158da21d1cb25751d863
ShlokSagarVIT <shlok.25bai10567@vitbhopal.ac.in>
2026-09-13 21:41:07 +0530

    Update Task.java

--- model/Task.java
+++ model/Task.java
@@ -1,5 +1,13 @@
-package productivity.model;
+package model;
 
 public class Task {
+
+    private int taskId;
+    private String title;
+    private String description;
+    private String category;
+    private String priority;
+    private String deadline;
+    private String status;
 
 }

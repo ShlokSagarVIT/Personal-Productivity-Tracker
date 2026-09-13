bde870a17047709a1b96384c8dd219676df68432
ShlokSagarVIT <shlok.25bai10567@vitbhopal.ac.in>
2026-09-13 21:59:42 +0530

    Update TaskManager.java

--- service/TaskManager.java
+++ service/TaskManager.java
@@ -1,5 +1,14 @@
-package productivity.service;
+package service;
+
+import model.Task;
+import java.util.ArrayList;
 
 public class TaskManager {
+
+    private ArrayList<Task> tasks;
+
+    public TaskManager() {
+        tasks = new ArrayList<>();
+    }
 
 }

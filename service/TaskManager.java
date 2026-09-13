62fc0d7ef862d645ff579ed04b281381e01b9d58
ShlokSagarVIT <shlok.25bai10567@vitbhopal.ac.in>
2026-09-13 22:03:23 +0530

    Update TaskManager.java

--- service/TaskManager.java
+++ service/TaskManager.java
@@ -7,8 +7,27 @@ public class TaskManager {
 
     private ArrayList<Task> tasks;
 
-    public TaskManager() {
-        tasks = new ArrayList<>();
+public TaskManager() {
+    tasks = new ArrayList<>();
+}
+
+public void addTask(Task task) {
+    tasks.add(task);
+}
+public void viewTasks() {
+    if (tasks.isEmpty()) {
+        System.out.println("No tasks available.");
+        return;
     }
 
+    for (Task task : tasks) {
+        System.out.println("Task ID: " + task.getTaskId());
+        System.out.println("Title: " + task.getTitle());
+        System.out.println("Category: " + task.getCategory());
+        System.out.println("Priority: " + task.getPriority());
+        System.out.println("Deadline: " + task.getDeadline());
+        System.out.println("Status: " + task.getStatus());
+        System.out.println("----------------------------");
+    }
+}
 }

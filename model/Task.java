53d8f5a49564eaa746fcc95060102146446d4174
ShlokSagarVIT <shlok.25bai10567@vitbhopal.ac.in>
2026-09-13 21:43:37 +0530

    Update Task.java

--- model/Task.java
+++ model/Task.java
@@ -1,13 +1,26 @@
 package model;
 
 public class Task {
-
-    private int taskId;
+     private int taskId;
     private String title;
     private String description;
     private String category;
     private String priority;
     private String deadline;
     private String status;
+
+
+public Task(int taskId, String title, String description,
+            String category, String priority, String deadline,
+            String status) {
+
+    this.taskId = taskId;
+    this.title = title;
+    this.description = description;
+    this.category = category;
+    this.priority = priority;
+    this.deadline = deadline;
+    this.status = status;
+}
 
 }
